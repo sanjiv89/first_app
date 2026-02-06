db2b228b2d94dc8e143d0ec0239a0e4c01524aed
sanjiv89 <36396782+sanjiv89@users.noreply.github.com>
2026-02-06 17:08:52 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BarOrderApp from './bar-order-app.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BarOrderApp />
+  </React.StrictMode>,
+)
