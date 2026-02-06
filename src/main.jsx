8357ba9b93e5f3cec1fcb260f118ec1049deabeb
sanjiv89 <36396782+sanjiv89@users.noreply.github.com>
2026-02-06 17:56:25 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
-test
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BarOrderApp from './BarOrderApp.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BarOrderApp />
+  </React.StrictMode>,
+)
